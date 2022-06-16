38a38ba7100b249bc6a9e37c9e142e68fc8a4471
zhaizhiwei1 <zhaizhiwei1@jd.com>
2022-06-16 10:08:29 +0800

    init

---
+++ gateway/src/main/java/com/self/GWApplication.java
@@ -0,0 +1,12 @@
+package com.self;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GWApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GWApplication.class);
+    }
+}
